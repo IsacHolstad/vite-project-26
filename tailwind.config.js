c7bcefa31d21262bd38196b0ecd2076158c42030
IsacHolstad <wisac22@gmail.com>
2022-09-01 11:44:40 +0200

    margin for xs mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'xs-mobile': '320px',
+                // => @media (min-width: 320px) { ... }
+                's-movile': '375px',
+                // => @media (min-width: 375px) { ... }
+                'm-mobile': '480', 
+                // => @media (min-width: 480px) { ... }
+              },
+        },
     },
     plugins: [],
 }
