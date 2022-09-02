2544a1972b7e01b6a7635c93c42317a42cad4da3
IsacHolstad <wisac22@gmail.com>
2022-09-02 10:43:06 +0200

    Remove node_modules folder

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            screens: {
+                'xs-mobile': '320px',
+                // => @media (min-width: 320px) { ... }
+                's-movile': '375px',
+                // => @media (min-width: 375px) { ... }
+                'm-mobile': '480', 
+                // => @media (min-width: 480px) { ... }
+                'desktop': {'min': '768px', 'max': '1440px'},
+              },
+        },
+    },
+    plugins: [],
+}
